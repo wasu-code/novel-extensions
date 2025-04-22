68f718db459a1d189f75a0545c62bc8b3f0add6d
Bigrand <bigr4nd@gmail.com>
2025-04-22 18:24:51 +0000

    fix(syosetsu): update CSS selectors, add new listings, fix description parsing

--- src/ja/Syosetsu.lua
+++ src/ja/Syosetsu.lua
@@ -1,13 +1,11 @@
--- {"id":3,"ver":"1.2.4","libVer":"1.0.0","author":"Doomsdayrs","dep":["url>=1.0.0"]}
---- @author Doomsdayrs
---- @version 1.2.0
+-- {"id":3,"ver":"1.2.5","libVer":"1.0.0","author":"Doomsdayrs","dep":["url>=1.0.0"]}
 
 local baseURL = "https://yomou.syosetu.com"
 local passageURL = "https://ncode.syosetu.com"
 local encode = Require("url").encode
 
 local function getTotalPages(html)
-	local lastPageLink = html:select("a.novelview_pager-last"):attr("href")
+	local lastPageLink = html:select(".c-pager__item--last"):attr("href")
 	if lastPageLink then
 		local totalPages = tonumber(lastPageLink:match("p=(%d+)"))
 		return totalPages or 1
@@ -25,33 +23,92 @@ local function expandURL(url)
 	return passageURL .. url
 end
 
+local function search(data)
+	local url = baseURL .. "/search.php?&word=" .. encode(data[0]) .. "&type=re&p=" .. data[PAGE]
+	local document = GETDocument(url)
+
+
+	return map(document:select("div.searchkekka_box"), function(v)
+		local novel = Novel()
+		local e = v:selectFirst("div.novel_h"):selectFirst("a.tl")
+		novel:setLink(shrinkURL(e:attr("href")))
+		novel:setTitle(e:text())
+		return novel
+	end)
+end
+
+local function parseListing(filter, data)
+	if data[PAGE] == 0 then
+		data[PAGE] = 1
+	end
+
+	local url = baseURL .. filter .. data[PAGE]
+	local document = GETDocument(url)
+
+	return map(document:select("div.searchkekka_box"), function(v)
+		local novel = Novel()
+		local e = v:selectFirst("div.novel_h"):selectFirst("a.tl")
+		novel:setLink(shrinkURL(e:attr("href")))
+		novel:setTitle(e:text())
+		return novel
+	end)
+end
+
 return {
 	id = 3,
 	name = "Syosetsu",
 	baseURL = baseURL,
 	imageURL = "https://github.com/shosetsuorg/extensions/raw/dev/icons/Syosetsu.png",
+
 	listings = {
-		Listing("Latest", true, function(data)
-			if data[PAGE] == 0 then
-				data[PAGE] = 1
-			end
-			return map(GETDocument(
-					baseURL .. "/search.php?&search_type=novel&order_former=search&order=new&notnizi=1&p=" .. data[PAGE])
-					:select("div.searchkekka_box"), function(v)
-				local novel = Novel()
-				local e = v:selectFirst("div.novel_h"):selectFirst("a.tl")
-				novel:setLink(shrinkURL(e:attr("href")))
-				novel:setTitle(e:text())
-				return novel
-			end)
+		Listing("Most Weekly Views", true, function(data)
+			return parseListing("/search.php?type=re&order=weekly&p=", data)
+		end),
+		Listing("Latest Published", true, function(data)
+			return parseListing("/search.php?type=re&order=new&notnizi=1&p=", data)
+		end),
+		Listing("Most Bookmarked", true, function(data)
+			return parseListing("/search.php?type=re&order=favnovelcnt&p=", data)
+		end),
+		Listing("Most Reviews", true, function(data)
+			return parseListing("/search.php?type=re&order=reviewcnt&p=", data)
+		end),
+		Listing("Highest Overall Points", true, function(data)
+			return parseListing("/search.php?type=re&order=hyoka&p=", data)
+		end),
+		Listing("Highest Daily Points", true, function(data)
+			return parseListing("/search.php?type=re&order=dailypoint&p=", data)
+		end),
+		Listing("Highest Weekly Points", true, function(data)
+			return parseListing("/search.php?type=re&order=weeklypoint&p=", data)
+		end),
+		Listing("Highest Monthly Points", true, function(data)
+			return parseListing("/search.php?type=re&order=monthlypoint&p=", data)
+		end),
+		Listing("Highest Quarterly Points", true, function(data)
+			return parseListing("/search.php?type=re&order=quarterpoint&p=", data)
+		end),
+		Listing("Highest Yearly Points", true, function(data)
+			return parseListing("/search.php?type=re&order=yearlypoint&p=", data)
+		end),
+		Listing("Most Raters", true, function(data)
+			return parseListing("/search.php?type=re&order=hyokacnt&p=", data)
+		end),
+		Listing("Most Words", true, function(data)
+			return parseListing("/search.php?type=re&order=lengthdesc&p=", data)
+		end),
+		Listing("Oldest Published", true, function(data)
+			return parseListing("/search.php?type=re&order=generalfirstup&p=", data)
+		end),
+		Listing("Oldest Updates", true, function(data)
+			return parseListing("/search.php?type=re&order=old&p=", data)
 		end)
 	},
 
 	-- Default functions that had to be set
 	getPassage = function(chapterURL)
-		local e = first(GETDocument(passageURL .. chapterURL):select("div"), function(v)
-			return v:id() == "novel_contents"
-		end)
+		local document = GETDocument(passageURL .. chapterURL)
+		local e = document:selectFirst(".p-novel")
 		if not e then
 			return "INVALID PARSING, CONTACT DEVELOPERS"
 		end
@@ -64,15 +121,16 @@ return {
 		local novelPage = NovelInfo()
 		local document = GETDocument(passageURL .. novelURL)
 
-		novelPage:setAuthors({ document:selectFirst("div.novel_writername"):text():gsub("作者：", "") })
-		novelPage:setTitle(document:selectFirst("p.novel_title"):text())
+		novelPage:setAuthors({ document:selectFirst(".p-novel__author a"):text()})
+		novelPage:setTitle(document:selectFirst(".p-novel__title"):text())
 
 		-- Description
-		local e = first(document:select("div"), function(v)
-			return v:id() == "novel_color"
-		end)
+		local e = document:selectFirst(".p-novel__summary")
 		if e then
-			novelPage:setDescription(e:text():gsub("<br>\n<br>", "\n"):gsub("<br>", "\n"))
+			novelPage:setDescription(tostring(e):gsub("<br%s*/?>", "\n")
+			:gsub("<[^>]->", "")                     
+			:gsub("\n%s*\n+", "\n\n")       
+			:gsub("^%s+", ""):gsub("%s+$", ""))
 		end
 		-- Chapters
 		if loadChapters then
@@ -80,12 +138,17 @@ return {
 			local totalPages = getTotalPages(document)
 			for page = 1, totalPages do
 				local pageURL = novelURL .. "?p=" .. page
+
 				local pageDocument = GETDocument(passageURL .. pageURL)
-				map(pageDocument:select("dl.novel_sublist2"), function(v)
+				map(pageDocument:select(".p-eplist__sublist"), function(v)
 					local chap = NovelChapter()
-					chap:setTitle(v:selectFirst("a"):text())
-					chap:setLink(v:selectFirst("a"):attr("href"))
-					chap:setRelease(v:selectFirst("dt.long_update"):text())
+					local title = v:selectFirst("a"):text()
+					local link = v:selectFirst("a"):attr("href")
+					local release = v:selectFirst(".p-eplist__update"):text()
+
+					chap:setTitle(title)
+					chap:setLink(link)
+					chap:setRelease(release)
 					table.insert(chapters, chap)
 				end)
 			end
@@ -97,15 +160,5 @@ return {
 	shrinkURL = shrinkURL,
 	expandURL = expandURL,
 	getTotalPages = getTotalPages,
-	search = function(data)
-		return map(GETDocument(baseURL .. "/search.php?&word=" .. encode(data[0]) .. "&p=" .. data[PAGE])
-				:select("div.searchkekka_box"),
-				function(v)
-					local novel = Novel()
-					local e = v:selectFirst("div.novel_h"):selectFirst("a.tl")
-					novel:setLink(shrinkURL(e:attr("href")))
-					novel:setTitle(e:text())
-					return novel
-				end)
-	end,
+	search = search,
 }
