fa1844f0fa6fc2a5ee047bcfd5cea907b07b8736
wasu-code <61418403+wasu-code@users.noreply.github.com>
2025-05-11 17:18:46 +0200

    feat(AnyWeb): separate logic of Readability library

--- src/all/AnyWeb.lua
+++ src/all/AnyWeb.lua
@@ -1,4 +1,6 @@
--- {"id": 23119214, "ver": "0.0.1", "libVer": "1.0.0", "author": "wasu-code", "dep": []}
+-- {"id": 23119214, "ver": "0.0.1", "libVer": "1.0.0", "author": "wasu-code", "dep": ["Readability>=0.0.2"]}
+
+local parseArticle = Require("Readability").parse
 
 local function parseNovel(novelUrl, loadChapters)
   local doc = GETDocument(novelUrl)
@@ -20,32 +22,7 @@ end
 
 local function getPassage(chapterUrl)
   local doc = GETDocument(chapterUrl)
-  doc:select("script"):remove()
-  doc:select("style"):remove()
-  doc:select("head"):remove()
-
-  local body = doc:selectFirst("body")
-  body:attr("id", "shosetsu-content")
-
-  local script = [[
-    var styleBackup = []; /*backup shosetsu stylesheets*/
-    document.querySelectorAll("style").forEach(function(style) {
-      styleBackup.push(style.outerHTML);
-    });
-
-    var article = new Readability(document, {
-      keepClasses: true
-    }).parse();
-    document.getElementById("shosetsu-content").innerHTML = article.content;
-
-    styleBackup.forEach(function(style) {
-      document.head.insertAdjacentHTML("beforeend", style);
-    });
-  ]]
-  body:append('<script src="https://cdnjs.cloudflare.com/ajax/libs/readability/0.6.0/Readability.min.js" integrity="sha512-gUyZHlv5aSSuW4HZmQxiuWLagqsiddDmftdPnfgjzjfGlbYnK+Ukam3TAAqKnZ3JXdsaFEwx8o9HGRaj6++ZnA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>')
-  body:append('<script>' .. script .. '</script>')
-
-  return pageOfElem(body, true, "", true)
+  return pageOfElem(parseArticle(doc), true, "", true)
 end
 
 local function search(data)
