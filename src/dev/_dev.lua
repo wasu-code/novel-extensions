74b617286d57a2f2f3e6c57377e49cac5e72a219
wasu-code <61418403+wasu-code@users.noreply.github.com>
2025-08-14 22:49:55 +0200

    fix: dev extension breaks CI

--- src/dev/_dev.lua
+++ src/dev/_dev.lua
@@ -1,8 +1,8 @@
 -- {"id": 23119210, "ver": "0.0.0", "libVer": "1.0.0", "author": "wasu-code"}
 
 -- Source URL of the external Lua file you're working on to load dynamically  
--- ! **Change accordingly**
-local URL = "http://192.168.1.23:5500/src/dev/extension-template.lua"
+-- ! **Change accordingly** (eg. "http://ipv4:port/src/dev/extension-template.lua")
+local URL = "https://raw.githubusercontent.com/wasu-code/shosetsu-extensions/refs/heads/dev/src/dev/extension-template.lua"
 
 -- NOTE: This development extension uses the same ID as the target script.
 --       This means downloaded chapters will be saved in the same directory 
