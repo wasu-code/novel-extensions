9d429d49b6dde4a28e757feb195e33477abb91ca
wasu-code <61418403+wasu-code@users.noreply.github.com>
2025-08-20 00:11:50 +0200

    Revert "style"
    This reverts commit 4f774230ff5edbdf3b814c9f2dfa71c547ae01da.

--- src/dev/_dev.lua
+++ src/dev/_dev.lua
@@ -2,13 +2,13 @@
 
 -- Source URL of the external Lua file you're working on to load dynamically  
 -- ! **Change accordingly** (eg. "http://ipv4:port/src/dev/extension-template.lua")
-local URL = "http://192.168.1.23:5533/src/all/AnyWeb.lua"
+local URL = "https://raw.githubusercontent.com/wasu-code/shosetsu-extensions/refs/heads/dev/src/dev/extension-template.lua"
 
 -- NOTE: This development extension uses the same ID as the target script.
 --       This means the following:
---     → Downloaded chapters will be saved in the same directory 
+--     ! Downloaded chapters will be saved in the same directory 
 --       as the original extension's chapters.
---     → To take effect settings must be changed on the original extension,
+--     ! To take effect settings must be changed on the original extension,
 --       not in !dev extension
 
 -- Fetch and load the remote script as a Lua chunk
